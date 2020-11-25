21d3a3f8b8eab545c715248780fece763f51fae7
ewere chukwuma ebie <37642292+caleb-42@users.noreply.github.com>
2020-11-25 15:02:56 +0100

    login complete

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -31,4 +31,28 @@ module.exports = function auth(server) {
       res.jsonp({ status: "error", message: "something went wrong" });
     }
   });
+
+  server.post("/auth/login", async (req, res, next) => {
+    console.log(req.body);
+    try {
+      const person = req.body;
+      if (!(person && person.password && person.email))
+        return res.jsonp({ status: "error", message: "invalid request data" });
+      const userExist = DbHandler.find({
+        table: "users",
+        body: person,
+        query: "email"
+      });
+      if (!userExist)
+        return res.jsonp({ status: "error", message: "user does not exist" });
+
+      const token = await DbHandler.createUser(person);
+      res.jsonp({
+        data: token,
+        status: "success"
+      });
+    } catch (e) {
+      res.jsonp({ status: "error", message: "something went wrong" });
+    }
+  });
 };
