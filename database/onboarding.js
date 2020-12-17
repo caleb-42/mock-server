ace2753e8f9ea309cf08baed07e9c9239c03afe6
ewere chukwuma ebie <37642292+caleb-42@users.noreply.github.com>
2020-12-17 11:22:51 +0100

    correct birthdates

--- database/onboarding.js
+++ database/onboarding.js
@@ -148,7 +148,7 @@ module.exports = {
     {
       zodiac: "/capricon.png",
       name: "Capricorn",
-      date: { start: "Dec 22", end: "Jan 19" },
+      date: { start: "22/12", end: "19/1" },
       character: "You are a natural entrepreneur",
       traits: `Action-oriented, gritty, and determined, you break barriers and launch ventures.`,
       caps: `Popular Caps include:|Jeff Bezos (Founder Amazon)|Benjamin Franklin (US Founding Father)|J. R. R. Tolkien (Writer, Lord of the Rings)`
@@ -156,7 +156,7 @@ module.exports = {
     {
       zodiac: "/aquarius.png",
       name: "Aquarius",
-      date: { start: "Jan 20", end: "Feb 18" },
+      date: { start: "20/1", end: "18/2" },
       character: "You are a revolutionary",
       traits: `Nonconformist, innovative, and collaborative, you are a force for progress and impact.`,
       caps: `Popular Aquarians include:|Abraham Lincoln (16th US President)|Wolfgang Mozart (Composer & Prodigy)|Rosa Parks (Activist)`
@@ -164,7 +164,7 @@ module.exports = {
     {
       zodiac: "/pisces.png",
       name: "Pisces",
-      date: { start: "Feb 19", end: "Mar 20" },
+      date: { start: "19/2", end: "20/3" },
       character: "Your imagination is a superpower!",
       traits: `Creative, natural learner, intuitive, you redefine and create without limits.`,
       caps: `Popular Pisces include:|Albert Einstein (Physicist)|Ruth Bader Ginsburg (US Justice)|Steve Jobs (Co-Founder Apple)`
@@ -172,7 +172,7 @@ module.exports = {
     {
       zodiac: "/aries.png",
       name: "Aries",
-      date: { start: "Mar 21", end: "Apr 19" },
+      date: { start: "21/3", end: "19/4" },
       character: "You are made to be the best!",
       traits: `Audacious, passionate and determined, you are made for challenges and leadership.`,
       caps: `Popular Aries include:|Maisie Williams (Arya, Game of Thrones)|Leonardo Da Vinci (Painter & Scientist)|Robert Downey Jr. (Actor, Iron Man)
@@ -181,7 +181,7 @@ module.exports = {
     {
       zodiac: "/taurus.png",
       name: "Taurus",
-      date: { start: "Apr 20", end: "May 20" },
+      date: { start: "20/4", end: "20/5" },
       character: "Your focus is unstoppable!",
       traits: `Consistent and reliable, you drive towards and connect with what matters most in life.`,
       caps: `Popular Taureans include:|Queen Elizabeth II of England|William Shakespeare (Writer)|Dwayne “Rock” Johnson. (Actor)`
@@ -189,7 +189,7 @@ module.exports = {
     {
       zodiac: "/gemini.png",
       name: "Gemini",
-      date: { start: "May 21", end: "Jun 20" },
+      date: { start: "21/5", end: "20/6" },
       character: "You are a true intellectual!",
       traits: `Curious, passionate and energetic, you can enjoy everything life has to offer.`,
       caps: `Popular Geminis include:|Marilyn Monroe (Actor)|Che Guevara (Revolutionary)|Kendrick Lamar (Musician)`
@@ -197,7 +197,7 @@ module.exports = {
     {
       zodiac: "/cancer.png",
       name: "Cancer",
-      date: { start: "Jun 21", end: "Jul 22" },
+      date: { start: "21/6", end: "22/7" },
       character: "Your intuition is a superpower!",
       traits: `Perceptive, observant and genuine, your tangible energy is a force for change.`,
       caps: `Popular Cancers include:|Nelson Mandela (Activist, President)|Ariana Grande (Musician)|Giorgio Armani (Designer)`
@@ -205,7 +205,7 @@ module.exports = {
     {
       zodiac: "/leo.png",
       name: "Leo",
-      date: { start: "Jul 23", end: "Aug 22" },
+      date: { start: "23/7", end: "22/8" },
       character: "You are a natural influencer!",
       traits: `Lively, theatrical, and daring, you are able to get attention and bring it on 100%.`,
       caps: `Popular Leos include:|Amelia Earhart (Aviator)|Barack Obama (44th US President)|Henry Ford (Industrialist)`
@@ -213,7 +213,7 @@ module.exports = {
     {
       zodiac: "/virgo.png",
       name: "Virgo",
-      date: { start: "Aug 23", end: "Sep 22" },
+      date: { start: "23/8", end: "22/9" },
       character: "Your mind is a supercomputer!",
       traits: `Diligent, detail-oriented, and resourceful,   you are a practical problem solver.`,
       caps: `Popular Virgos include:|Warren Buffet (Billionaire Investor)|Kobe Bryant (Basketballer)|Jimmy Fallon (Comedian)`
@@ -221,7 +221,7 @@ module.exports = {
     {
       zodiac: "/libra.png",
       name: "Libra",
-      date: { start: "Sep 23", end: "Oct 22" },
+      date: { start: "23/9", end: "22/10" },
       character: `You are masterful at execution!`,
       traits: `Intellectualism, attention to detail and high standards enables you create new initiatives.`,
       caps: `Popular Libras include:|Serena Williams (Tennis Star)|Mahatma Gandhi (Activist, Lawyer)|Matt Damon (Actor)`
@@ -229,7 +229,7 @@ module.exports = {
     {
       zodiac: "/scorpio.png",
       name: "Scorpio",
-      date: { start: "Oct 23", end: "Nov 21" },
+      date: { start: "23/10", end: "21/11" },
       character: `You are a grandmaster!`,
       traits: `Strategic thinking, emotional depth and hardwork allow you win the long game.`,
       caps: `Popular Scorpios include:|Bill Gates (Co-Founder Microsoft)|Theodore Roosevelt (26th US President)|Marie Curie (1st Female Nobel Scientist)`
@@ -237,11 +237,10 @@ module.exports = {
     {
       zodiac: "/sagittarius.png",
       name: "Sagittarius",
-      date: { start: "Nov 22", end: "Dec 21" },
+      date: { start: "22/11", end: "21/12" },
       character: `You are a go-getter!`,
       traits: `Fearless, intelligent, and adaptable, life is one big adventure which you can conquer.`,
-      caps: `
-      Popular Sags include:|Walt Disney (Animator)|Chadwick Boseman (Actor, Black Panther)|Jane Austen (Novelist)`
+      caps: `Popular Sags include:|Walt Disney (Animator)|Chadwick Boseman (Actor, Black Panther)|Jane Austen (Novelist)`
     }
   ]
 };
